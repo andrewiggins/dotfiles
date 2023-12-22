c84202d58d458352398b0b7a20ba319fee3a2805
Andre Wiggins <459878+andrewiggins@users.noreply.github.com>
2023-12-21 19:11:57 -0800

    Fix issues discovered in GH codespaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,28 @@
-#!/usr/bing/env bash
+#!/usr/bin/env bash
 set -e
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+# Ensure latest dotfiles are used
 git pull origin main;
 
-curl -sS https://starship.rs/install.sh | sh
+# Install starship prompt
+curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
+cat >> "$HOME/.bashrc" <<- 'EOF'
 
+	# Init starship prompt
+	eval "$(starship init bash)"
+
+	# Set window title to output of starship directory module
+	function set_win_title() {
+		echo -ne "\033]0; $(echo $(starship module directory) | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g") \007"
+	}
+
+	starship_precmd_user_func="set_win_title"
+EOF
+
+# Install volta
 curl https://get.volta.sh | bash
-volta install node
+
+# Load .bashrc
+source ~/.bashrc
